9e6986c0a99509d4d3e7c527e4c47bc5ace0f614
danny0ceanCA <danielgojeda.1@gmail.com>
2025-06-13 15:29:52 -0700

    Delete frontend directory

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-const e = React.createElement;
-
-function App() {
-  return e('div', null, 'Hello from React');
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(e(App));
