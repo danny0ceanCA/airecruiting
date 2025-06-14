ece6d090d2b1192a24bf739f5ac754f02f0b11b9
danny0ceanCA <danielgojeda.1@gmail.com>
2025-06-14 09:30:36 -0700

    Update LoginForm.js

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios/dist/node/axios.cjs';
+import axios from "axios";
 import { Link } from 'react-router-dom';
 import './LoginForm.css';
 
