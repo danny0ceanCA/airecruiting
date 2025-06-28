fe5a2db4a35e1b1f14529e920e50742400cbf72d
daniel0ceanCA <danielgojeda.1@gmail.com>
2025-06-28 08:00:10 -0700

    updated school codes

--- backend/app/school_codes.py
+++ backend/app/school_codes.py
@@ -2,4 +2,5 @@ SCHOOL_CODE_MAP = {
     "1001": "1001-Unitek-Sacramento",
     "1002": "1002-Unitek-SanJose",
     "2001": "2001-Summit-Riverside",
+    "3001": "200-Maxim Recruiting Services-San Francisco"
 }
