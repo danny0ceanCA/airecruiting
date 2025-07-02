4998fe0c441496f5b4ecde28e76e74ea4eeba040
danny0ceanCA <danielgojeda.1@gmail.com>
2025-07-02 14:27:36 -0700

    refactor: unify navigation menu

--- frontend/src/AdminMenu.js
+++ frontend/src/AdminMenu.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
+import './AdminMenu.css';
 
 function AdminMenu({ children }) {
   const [menuOpen, setMenuOpen] = useState(false);
