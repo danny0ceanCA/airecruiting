7df3a16ab4348b3ddd75c5b214a48da4597c950b
danny0ceanCA <danielgojeda.1@gmail.com>
2025-07-05 00:25:10 -0700

    Fix institution code labels

--- backend/app/school_codes.py
+++ backend/app/school_codes.py
@@ -1,7 +1,7 @@
 SCHOOL_CODE_MAP = {
-    "1001": "1002-Unitek-Sacramento",
-    "1002": "1004-Unitek-SanJose",
+    "1001": "1001-Unitek-Sacramento",
+    "1002": "1002-Unitek-SanJose",
     "1003": "1003-Unitek-Concord",
     "2001": "2001-Summit-Riverside",
-    "3001": "Maxim Recruiting Services-San Francisco"
+    "3001": "Maxim Recruiting Services-San Francisco",
 }
