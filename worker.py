8ec5baf8a51d4ca5d8af75efc562418077e9de2c
danny0ceanCA <danielgojeda.1@gmail.com>
2025-07-31 08:32:52 -0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,8 +1,7 @@
 import os
 import redis
 
-from rq import Worker, Queue
-from rq.connections import Connection
+from rq import Worker, Queue, Connection
 
 redis_url = os.getenv("REDIS_URL")
 if not redis_url:
