4d0edac8c8f2fcb8ef8f423b4795a11fc8874f4f
danny0ceanCA <danielgojeda.1@gmail.com>
2025-08-17 13:21:30 -0700

    feat: enhance top menu visuals

--- frontend/src/TopMenu.js
+++ frontend/src/TopMenu.js
@@ -5,11 +5,26 @@ import './TopMenu.css';
 function TopMenu() {
   return (
     <nav className="top-menu">
-      <Link to="/">Home</Link>
-      <Link to="/about">About TalentMatch-AI</Link>
-      <Link to="/about/applicants">Applicants</Link>
-      <Link to="/about/career-service">Career Service</Link>
-      <Link to="/about/recruiters">Recruiters</Link>
+      <Link to="/">
+        <span className="material-icons">home</span>
+        Home
+      </Link>
+      <Link to="/about">
+        <span className="material-icons">info</span>
+        About TalentMatch-AI
+      </Link>
+      <Link to="/about/applicants">
+        <span className="material-icons">person</span>
+        Applicants
+      </Link>
+      <Link to="/about/career-service">
+        <span className="material-icons">work</span>
+        Career Service
+      </Link>
+      <Link to="/about/recruiters">
+        <span className="material-icons">group</span>
+        Recruiters
+      </Link>
     </nav>
   );
 }
